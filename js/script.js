73fb1203895b06786f8642ddd895c2aaaba77201
KarlaSz <128708615+KarlaSz@users.noreply.github.com>
2023-09-06 19:41:53 +0200

    footer and style, finish

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector(".nav");
 const navBtn = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
+const navBtnBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
   nav.classList.toggle("nav--active");
+
+  navBtnBars.classList.remove("black-bars-color");
 
   allNavItems.forEach((item) => {
     item.addEventListener("click", () => {
@@ -23,4 +28,28 @@ const handleNavItemsAnimation = () => {
   });
 };
 
+const handleObserver = () => {
+  const currentSection = window.scrollY;
+  allSections.forEach((section) => {
+    if (
+      section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.add("black-bars-color");
+    } else if (
+      !section.classList.contains("white-section") &&
+      section.offsetTop <= currentSection + 60
+    ) {
+      navBtnBars.classList.remove("black-bars-color");
+    }
+  });
+};
+
+const handleCurrentYear = () => {
+  const year = new Date().getFullYear();
+  footerYear.innerText = year;
+};
+
+handleCurrentYear();
 navBtn.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleObserver);
